c0072a56670d1da77a9f9dd2d5b40d31f0f2589b
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-12 17:33:53 +0100

    rad

--- ZavrsniRad/src/brkic/ljetnizadatak/Start.java
+++ ZavrsniRad/src/brkic/ljetnizadatak/Start.java
@@ -70,11 +70,21 @@ public class Start {
 	}
 
 	private void korisnikBrisanje() {
-		
+		korisnikStravke("Trenutno dostupno u aplikaciji");
+		int redniBroj  = Ulaz.ucitajInt("Odaberite redni broj za brisanje: ", 
+				"Niste unijeli cijeli broj", 1, korisnici.size());
+		korisnici.remove(redniBroj-1);
+		korisnikIzbornik();
 	}
 
 	private void korisnikPromjena() {
-		
+		korisnikStravke("Trenutno dostupno u aplikaciji");
+		int redniBroj = Ulaz.ucitajInt("Odaberite redni broj za promjenu: ",
+				"Niste unijeli cijeli broj", 1, korisnici.size());
+		Korisnik korisnikZaPromjenu = korisnici.get(redniBroj-1);
+		korisnikZaPromjenu = korisnikPostaviVrijednosti(korisnikZaPromjenu);
+		korisnici.set(redniBroj-1, korisnikZaPromjenu);
+		korisnikIzbornik();
 	}
 
 	private void korisnikUnosNovog() {
@@ -88,10 +98,37 @@ public class Start {
 		k.setSifra(Ulaz.ucitajInt("Unesite sifru: ",
 				"sifra mora biti cijeli broj",
 				1, Integer.MAX_VALUE));
+		k.setIme(Ulaz.ucitajString("Unesite ime korisnika; ",
+				"Ime obavezno"));
+		k.setPrezime(Ulaz.ucitajString("Unesi prezime korisnika: ",
+				"Prezime obavezno"));
+		k.setEmail(Ulaz.ucitajString("Unesi email korisnika: ",
+				"Email obavezan"));
+		k.setBrojClanskeKartice(Ulaz.ucitajInt("Unesi broj clanske kartice korisnika: ",
+				"Broj kartice obavezan", 1, 9999));
 		return k;
 	}
 
 	private void korinsikPregled() {
+		korisnikStravke("Pregled unesenih smjerova");
+		korisnikIzbornik();
+	}
+
+	private void korisnikStravke(String naslov) {
+		System.out.println(naslov);
+		System.out.println("--------------------");
+		if(korisnici.size()==0) {
+			System.out.println("Nema unesenih korisnika");
+		}else {
+			Korisnik k;
+			for(int i=0;i<korisnici.size();i++) {
+				k = korisnici.get(i);
+				System.out.println((i + 1) + ". " + k.getIme() +
+						" " + k.getPrezime() +
+						" " + k.getEmail() + 
+						" " + k.getBrojClanskeKartice());
+			}
+		}
 		
 	}
 
