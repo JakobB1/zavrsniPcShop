129af9edf833d923eab237b51bb7c2e0bc4bda69
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-12 17:22:27 +0100

    rad

--- ZavrsniRad/src/brkic/ljetnizadatak/Start.java
+++ ZavrsniRad/src/brkic/ljetnizadatak/Start.java
@@ -2,6 +2,8 @@ package brkic.ljetnizadatak;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Start {
 	
@@ -9,6 +11,8 @@ public class Start {
 	
 	public Start() {
 		korisnici = new ArrayList<Korisnik>();
+		
+		Ulaz.scanner = new Scanner(System.in);
 		glavniIzbornik();
 	}
 	
@@ -25,6 +29,69 @@ public class Start {
 	}
 
 	private void ucitajGlavnuAkciju() {
+		switch (Ulaz.ucitajInt("Odaberite stavku: ", "Niste unijeli cijeli broj", 1, 5)) {
+		case 1:
+			korisnikIzbornik();
+			break;
+		case 5:
+			System.out.println("Program je zavrsio, dovidenja!");
+			return;
+		}
+	}
+	
+	
+	 
+    ///////////
+    //// POCETAK KORISNIK
+    ///////////
+
+	private void korisnikIzbornik() {
+		System.out.println("--------------------");	
+		System.out.println("Podizbornik 1. Korisnici");		
+		System.out.println("Odaberite akciju");
+		System.out.println(" 1. Pregled unesenih korisnika");
+		System.out.println(" 2. Unos novog korisnika");
+		System.out.println(" 3. Promjena postojeceg korisnika");
+		System.out.println(" 4. Brisanje korisnika");
+		System.out.println(" 5. Povratak u prethodni izbornik");
+		
+		ucitajIzbornikAkciju();
+	}
+
+	private void ucitajIzbornikAkciju() {
+		switch(Ulaz.ucitajInt("Odaberite akciju: ",
+				"Niste unijeli cijeli broj", 1, 5)) {
+		case 1 -> korinsikPregled();
+		case 2 -> korisnikUnosNovog();
+		case 3 -> korisnikPromjena();
+		case 4 -> korisnikBrisanje();
+		case 5 -> glavniIzbornik();
+		}
+	}
+
+	private void korisnikBrisanje() {
+		
+	}
+
+	private void korisnikPromjena() {
+		
+	}
+
+	private void korisnikUnosNovog() {
+		Korisnik k = new Korisnik();
+		k = korisnikPostaviVrijednosti(k);
+		korisnici.add(k);
+		korisnikIzbornik();
+	}
+
+	private Korisnik korisnikPostaviVrijednosti(Korisnik k) {
+		k.setSifra(Ulaz.ucitajInt("Unesite sifru: ",
+				"sifra mora biti cijeli broj",
+				1, Integer.MAX_VALUE));
+		return k;
+	}
+
+	private void korinsikPregled() {
 		
 	}
 
--- ZavrsniRad/src/brkic/ljetnizadatak/Ulaz.java
+++ ZavrsniRad/src/brkic/ljetnizadatak/Ulaz.java
@@ -29,4 +29,20 @@ public class Ulaz {
 		}
 		return broj;
 	}
+	
+	public static String ucitajString(
+			String poruka, 
+			String greska) {
+		String s = "";
+		while(true) {
+			System.out.print(poruka);
+			s = scanner.nextLine();
+			if(s.trim().equals("")) {
+				System.out.println(greska);
+				continue;
+			}
+			break;
+		}
+		return s;
+	}
 }
