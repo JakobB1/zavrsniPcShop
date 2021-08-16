a8930b158269c4c4d9219a7889a7b54861230b29
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-16 13:29:53 +0100

    djelatnik

---
+++ ZavrsniRad/src/console/Djelatnik.java
@@ -0,0 +1,44 @@
+package console;
+
+public class Djelatnik {
+	
+	private int sifra;
+	private String ime;
+	private String prezime;
+	private String brojClanskeKartice;
+	private String email;
+	
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getBrojClanskeKartice() {
+		return brojClanskeKartice;
+	}
+	public void setBrojClanskeKartice(String brojClanskeKartice) {
+		this.brojClanskeKartice = brojClanskeKartice;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
