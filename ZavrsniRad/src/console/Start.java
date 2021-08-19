5a2d2bae39a322a0d8ca3e7853bb10a88d484709
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-19 13:23:22 +0100

    vrijednosti

--- ZavrsniRad/src/console/Start.java
+++ ZavrsniRad/src/console/Start.java
@@ -391,11 +391,10 @@ public class Start {
 		p.setCijena(Ulaz.ucitajDouble("Unesi cijenu: ", 
 				"Cijena mora biti decimalni broj", 0, 100000));
 		p.setGarancija(Ulaz.ucitajDatum("Unesi datum pocetka: "));
-		p.setOpisProizvoda(Ulaz.ucitajString("Unesite opis proizvoda", "Opis obavezan"));
+		p.setOpisProizvoda(Ulaz.ucitajString("Unesite opis proizvoda: ", "Opis obavezan"));
 		p.setDostupnostZaKupnju(Ulaz.ucitajBoolean("Unesite oznaku da li je " 
 		+ "proizvod certificiran: " , "Kriva vrijednost" ));
-		p.setKodArtikla(Ulaz.ucitajString("Unesite kod artikla", "Kod obavezan"));
-        //// nastavak
+		p.setKodArtikla(Ulaz.ucitajString("Unesite kod artikla: ", "Kod obavezan"));
 		return p;
 	}
 
